6c73c0c317534d181296113b868064ef6f1abc6d
Miroslav Biňas <miroslav.binas@tuke.sk>
2020-02-01 01:16:43 +0100

    prvy pokus na pracu s mapou

--- project/game.py
+++ project/game.py
@@ -1,10 +1,54 @@
-HEIGHT = 800
-WIDTH = 600
+#from zeromap import ZeroMap
+import pytmx
+from time import sleep
+
+
+HEIGHT = 600
+WIDTH = 800
+
+class View():
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+tmx = None
+view = View(70, 42, 23, 13)
+
 
 
 def draw():
-    pass
+    print('>> draw')
+    global tmx, view
+    screen.blit('screen', (0, 0))
+    #print(viewport[1])
+    #row = 10
+
+    #for row in range(view, viewport[1] + HEIGHT//tmx.tileheight):
+    #    for col in range(viewport[0], viewport[0] + WIDTH//tmx.tilewidth):
+
+    y = 0
+    for row in range(view.y, view.y + view.height):
+        x = 0
+        for col in range(view.x, view.x + view.width):
+            image = tmx.get_tile_image(col, row, 0)
+            screen.blit(image, (32 + x*tmx.tilewidth, 160+y*tmx.tileheight))
+            x+= 1
+        y+=1
 
 
 def update():
-    pass
+    print('>> update')
+    global view
+    view.x += 1
+
+
+def main():
+    print('>> main')
+    global tmx
+    tmx = pytmx.load_pygame('maps/magicland.tmx')
+
+
+
+main()
---
+++ project/maps/tiles2x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="tiles2x" tilewidth="32" tileheight="32" tilecount="1274" columns="49">
+ <image source="tiles2x.png" width="1580" height="860"/>
+</tileset>
